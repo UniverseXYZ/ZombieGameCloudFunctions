05bf65c94e51e6cbd4efb81daffeee783ead10d5
valeri.ivanchev@limechain.tech <valeri.ivanchev@limechain.tech>
2022-08-18 13:33:43 +0300

    Generate code api

--- functions/src/api/user/user.service.ts
+++ functions/src/api/user/user.service.ts
@@ -31,6 +31,26 @@ export class UserService {
       });
   }
 
+  async getUserId(walletAddress: string) {
+    const hasPoly = await this.getUserHasPolyMorphs(walletAddress);
+    if (hasPoly) {
+      return UserModel.findOneAndUpdate({
+        'walletAddress': walletAddress,
+      }, {
+        walletAddress,
+        id: Math.random().toString(36).substring(2, 12).toUpperCase(),
+        idIsUsed: false,
+      }, {
+        upsert: true,
+        new: true,
+      })
+        .exec()
+        .then((user) => user);
+    }
+
+    return null;
+  }
+
   private getUser(id: string) {
     return UserModel.findOne({
       'id': id,
