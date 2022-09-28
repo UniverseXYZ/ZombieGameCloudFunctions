a934f40742f936aa2ddf1e4007602f73d6177575
Valeri Ivanchev <valeri.ivanchev@limechain.tech>
2022-09-28 14:22:27 +0300

    Limited deviants support (#14)
    * Limited deviants support
    
    * Refactor the number of deviants
    
    * Update yaml
    
    * Add metadata to the secrets

--- functions/src/api/user/user.service.ts
+++ functions/src/api/user/user.service.ts
@@ -3,7 +3,7 @@ import { providers } from 'ethers';
 import request from 'graphql-request';
 import { GetUserEligibleRespose, MetaDataResponse, SetUserScoreArgs, TheGraphResponse } from './user';
 import { UserModel } from './user.model';
-import { GET_POLYMORPHS_QUERY } from './user.queries';
+import { GET_DEVIANTS_QUERY, GET_POLYMORPHS_QUERY } from './user.queries';
 const flatten = require('flat');
 
 export class UserService {
@@ -88,7 +88,11 @@ export class UserService {
       walletAddress,
     }));
 
-    return theGraphV2Response.transferEntities.length > 0;
+    const theGraphDeviantsResponse = <TheGraphResponse>(await request(<string>process.env.THE_GRAPH_DEVIANTS_URL, GET_DEVIANTS_QUERY, {
+      walletAddress,
+    }));
+
+    return theGraphV2Response.transferEntities.length > 0 || theGraphDeviantsResponse.transferEntities.length > 0;
   }
 
 
@@ -130,11 +134,16 @@ export class UserService {
       walletAddress,
     }));
 
+    const theGraphDeviantsResponse = <TheGraphResponse>(await request(<string>process.env.THE_GRAPH_DEVIANTS_URL, GET_DEVIANTS_QUERY, {
+      walletAddress,
+    }));
+
     // const v1Ids = theGraphV1Response.transferEntities.map((entity) => entity.tokenId);
     const v2Ids = theGraphV2Response.transferEntities.map((entity) => entity.tokenId);
-
+    const deviantsIds = theGraphDeviantsResponse.transferEntities.map((entity) => entity.tokenId);
     // let metadataV1 = [];
     let metadataV2 = [];
+    const metadataDeviants = [];
 
     // if (v1Ids.length > 0) {
     //   metadataV1 = await this.getMetadataFromTokenIds(v1Ids.join(), <string>process.env.THE_GRAPH_V1_GET_METADATA);
@@ -144,7 +153,13 @@ export class UserService {
       metadataV2 = await this.getMetadataFromTokenIds(v2Ids.join(), <string>process.env.THE_GRAPH_V2_GET_METADATA);
     }
 
-    return metadataV2;
+    const numberOfDeviants = deviantsIds.length > 5 ? 5 : deviantsIds.length;
+    for (let i = 0; i < numberOfDeviants; i++) {
+      const metadata = await this.getDeviantsMetadataFromTokenId(deviantsIds[i]);
+      metadataDeviants.push(metadata);
+    }
+
+    return metadataV2.concat(metadataDeviants);
   }
 
   getLeaderboard() {
@@ -161,6 +176,26 @@ export class UserService {
       .sort({ score: -1, _id: 1 })
       .exec()
       .then((users) => users.findIndex((user) => user.walletAddress === walletAddress) + 1);
+  }
+
+  private getDeviantsMetadataFromTokenId(tokenId: string) {
+    return axios({
+      method: 'GET',
+      url: <string>process.env.GET_DEVIANTS_METADATA,
+      params: {
+        id: tokenId,
+      },
+    })
+      .then((res) => {
+        console.log(res.data);
+        const metadata = res.data;
+
+        return {
+          name: metadata.name,
+          type: metadata.attributes.find((attribute: any) => attribute.trait_type == 'Species').value,
+          imageUrl: metadata.image,
+        };
+      });
   }
 
   private getMetadataFromTokenIds(tokenIds: string, url: string) {
